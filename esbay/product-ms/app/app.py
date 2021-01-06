d936568f825e75383a5c235927a312961b01d4ee
jmaisonnave <jeanmaisonnave@wanadoo.fr>
2021-01-06 16:22:09 +0100

    added creation of products and users

--- esbay/product-ms/app/app.py
+++ esbay/product-ms/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, json, request, jsonify
 import models
 
 app = Flask(__name__)
@@ -11,9 +11,40 @@ app.config.update(
 
 models.init_app(app)
 models.create_tables(app)
-@app.route('/products/welcome')
+
+@app.route('/product/welcome')
 def hello():
     return 'Hello, welcome to the ESBAY Product API`\n'
+
+
+@app.route('product/create', methods=['POST'])
+def create_product():
+
+    name = request.form['name']
+    seller = request.form['seller']
+    price = request.form['price']
+
+    item = models.Product()
+    item.name = name
+    item.seller = seller
+    item.price = price
+
+    models.db.session.add(item)
+    models.db.session.commit()
+
+    response = jsonify({'message' : 'Product added', 'product': item.to_json})
+    return response
+
+@app.route('/api/products', method=['GET'])
+def get_products():
+    data = []
+
+    for row in models.Product.query.all():
+        data.append(row.to_json())
+
+    response = jsonify(data)
+
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
--- esbay/user-ms/app/app.py
+++ esbay/user-ms/app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, make_reponse, request, json, jsonify
+from passlib.hash import sha256_crypt
 import models
 
 app = Flask(__name__)
@@ -10,6 +11,33 @@ app.config.update(dict(
 
 models.init_app(app)
 models.create_tables(app)
+
+@app.route('/user/welcome')
+def hello():
+    return 'Hello, welcome to the ESBAY Product API`\n'
+
+@app.route('/api/user/create', methods=['POST'])
+def post_register():
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = sha256_crypt.hash((str(request.form['password'])))
+
+    user = model.User()
+    user.email = email
+    user.first_name = first_name
+    user.last_name = last_name
+    user.password = password
+    user.authenticated = True
+    user.active = True
+
+    models.db.session.add(user)
+    models.db.session.commit()
+
+    response = jsonify({'message': 'User added', 'result': user.to_json()})
+
+    return response
 
 @app.route('/api/users', method=['GET'])
 def get_users():
